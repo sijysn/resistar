1babd509e37ff60ebd3d70f62c2599735d344d53
yoshinoseiji <sij.ysn25@gmail.com>
2022-09-02 02:38:03 +0900

    feat: loginの定義を変更

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const jwtToken = request.cookies.get("jwt-token");
+  const jwtToken = request.cookies.get("jwtToken");
+  const userID = request.cookies.get("userID");
+  const isLoggedIn = jwtToken && userID;
   const isPathLogin = request.nextUrl.pathname === "/login";
-  if (jwtToken && isPathLogin) {
+  if (isLoggedIn && isPathLogin) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-  if (!jwtToken && !isPathLogin) {
+  if (!isLoggedIn && !isPathLogin) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
   return NextResponse.next();
