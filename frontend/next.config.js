ee22c14b91d933516b5b13a52978d75a7c497c4e
yoshinoseiji <sij.ysn25@gmail.com>
2022-09-25 21:26:23 +0900

    feat: PWAを実装

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+  // disable: process.env.NODE_ENV === "development",
+  register: true,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     emotion: true,
   },
-};
+});
 
 module.exports = nextConfig;
