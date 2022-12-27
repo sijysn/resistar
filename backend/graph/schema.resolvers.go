75e9859c6fdff7991fd9df5a529e4c51d5cb574c
yoshinoseiji <sij.ysn25@gmail.com>
2022-12-27 11:48:27 +0900

    refactor: groupsの責務を分解

--- backend/graph/schema.resolvers.go
+++ backend/graph/schema.resolvers.go
@@ -680,50 +680,9 @@ func (r *queryResolver) Users(ctx context.Context, input model.UsersQuery) ([]*m
 
 // Groups is the resolver for the groups field.
 func (r *queryResolver) Groups(ctx context.Context, input model.GroupsQuery) ([]*model.Group, error) {
-	responseAccess := ctx.Value(middleware.ResponseAccessKey).(*middleware.ResponseAccess)
-	if responseAccess.Status == http.StatusInternalServerError {
-		return nil, fmt.Errorf("サーバーエラーが発生しました")
-	}
-	var groups []*model.Group
-	if responseAccess.Status == auth.StatusUnauthorized {
-		errorMessage := "認証されていません"
-		groups = append(groups, &model.Group{
-			ErrorMessage: &errorMessage,
-		})
-		return groups, nil
-	}
-
-	var dbUser *dbModel.User
-	userID, err := strconv.ParseUint(input.UserID, 10, 64)
+	groups, err := r.Usecase.GetGroups(ctx, input)
 	if err != nil {
 		return nil, err
-	}
-	err = r.DB.Debug().Where("id = ?", uint(userID)).Preload("Groups").Limit(1).Find(&dbUser).Error
-	if err != nil {
-		return nil, err
-	}
-
-	for _, dbGroup := range dbUser.Groups {
-		// 所属しているグループのメンバーを取得する
-		var myGroupUsers []*model.User
-		var dbMyGroup *dbModel.Group
-		err = r.DB.Debug().Where("id = ?", uint(dbGroup.ID)).Preload("Users").Limit(1).Find(&dbMyGroup).Error
-		if err != nil {
-			return nil, err
-		}
-		for _, dbMyGroupUser := range dbMyGroup.Users {
-			myGroupUsers = append(myGroupUsers, &model.User{
-				ID:    strconv.FormatUint(uint64(dbMyGroupUser.ID), 10),
-				Email: dbMyGroupUser.Email,
-				Name:  dbMyGroupUser.Name,
-			})
-		}
-
-		groups = append(groups, &model.Group{
-			ID:    strconv.FormatUint(uint64(dbGroup.ID), 10),
-			Name:  dbGroup.Name,
-			Users: myGroupUsers,
-		})
 	}
 	return groups, nil
 }
