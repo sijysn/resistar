c04fb5248789c5a309e6da0828e905b7610fe4f2
yoshinoseiji <sij.ysn25@gmail.com>
2022-12-28 14:58:24 +0900

    refactor: logoutを分解

--- backend/graph/schema.resolvers.go
+++ backend/graph/schema.resolvers.go
@@ -556,57 +556,11 @@ func (r *mutationResolver) LoginGroup(ctx context.Context, input model.LoginGrou
 
 // LogoutGroup is the resolver for the logoutGroup field.
 func (r *mutationResolver) LogoutGroup(ctx context.Context) (*model.Result, error) {
-	responseAccess := ctx.Value(middleware.ResponseAccessKey).(*middleware.ResponseAccess)
-	if responseAccess.Status == http.StatusInternalServerError {
-		return nil, fmt.Errorf("サーバーエラーが発生しました")
-	}
-	if responseAccess.Status != auth.StatusGroup {
-		errorMessage := "認証されていません"
-		return &model.Result{
-			Message: errorMessage,
-			Success: false,
-		}, nil
-	}
-	signBytes, err := ioutil.ReadFile("./jwt.pem")
-	if err != nil {
-		panic(err)
-	}
-	session.Session.GroupID = nil
-
-	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(signBytes)
-	if err != nil {
-		panic(err)
-	}
-
-	sessionToken := utility.GenerateToken()
-	claims := jwt.MapClaims{
-		"sessionToken": sessionToken,
-		"userID":       &session.Session.UserID,
-		"exp":          time.Now().AddDate(0, 1, 0).Unix(),
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-	jwtToken, err := token.SignedString(signKey)
-	if err != nil {
-		panic(err)
-	}
-	responseAccess.SetCookie("jwtToken", jwtToken, false, time.Now().Add(24*time.Hour))
-	responseAccess.DeleteCookie("groupID")
-	responseAccess.Writer.WriteHeader(http.StatusOK)
-
-	loginLog := &dbModel.UserLoginLog{
-		UserID: *session.Session.UserID,
-		Token:  utility.SHA512(sessionToken),
-	}
-	err = r.DB.Create(loginLog).Error
+	result, err := r.Usecase.LogoutGroup(ctx)
 	if err != nil {
 		return nil, err
 	}
-
-	message := "グループからログアウトしました"
-	return &model.Result{
-		Message: message,
-		Success: true,
-	}, nil
+	return result, nil
 }
 
 // Histories is the resolver for the histories field.
