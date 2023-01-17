280c587313728c7e530d9b97957e54b24ca5b52a
yoshinoseiji <sij.ysn25@gmail.com>
2023-01-17 23:00:07 +0900

    feat: queryを実装

---
+++ frontend/lib/apollo/api/uploadProfileImage.ts
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const UPLOAD_PROFILE_IMAGE = gql`
+  mutation UploadProfileImage($data: Upload!) {
+    uploadProfileImage(input: { data: $data }) {
+      path
+    }
+  }
+`;
+
+export type uploadProfileImageProps = {
+  uploadProfileImage: {
+    path: string;
+  };
+};
+
+export type uploadProfileImageVarsProps = {
+  data: any;
+};
